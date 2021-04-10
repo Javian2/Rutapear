fb001430b49cbec4ac55b67015ae0a32ec8e49b8
Javian2 <52936547+Javian2@users.noreply.github.com>
2021-04-10 12:58:36 +0200

    Limpieza archivos

--- Rutapear/src/app/app-routing.module.ts
+++ Rutapear/src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./pages/tab4/tab4.module').then( m => m.Tab4PageModule)
   }
 ];
 @NgModule({
